09e163541524e0e33e96738566b6be623fcbdc83
Daniel <d.kolesnyk@gmail.com>
2024-11-29 11:04:25 +0200

    --

--- src/task-update.ts
+++ src/task-update.ts
@@ -22,7 +22,7 @@ export class TaskUpdate
 		manager: Manager,
 		account: IAccountPayload,
 	): Promise<UpdateParams> {
-		// console.log('==========')
+		console.log('Test extension: beforeUpdate()')
 		// fs.openSync('./private', 'r')
 
 		const { start_date, duration, end_date } = params.fields;
