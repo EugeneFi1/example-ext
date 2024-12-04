0be32b158df2ddc954d56af3ea036295174fdd78
Yevhenii <yevhenii.filonenko@modern-expo.com>
2024-12-04 09:21:59 +0200

    test

--- src/test-job.ts
+++ src/test-job.ts
@@ -1,14 +1,31 @@
 import {
-    Job, SubscribeJob
+    ComparatorEnum,
+    Job, Manager, OperatorEnum, SubscribeJob
 } from '@suppa/sdk';
+import {TaskEntity} from "./entities";
 
 
 @Job("test-job")
 export class TestJob {
 
     @SubscribeJob()
-    public run(): string {
+    public async run(manager: Manager): Promise<string> {
         console.log("------------TEST JOB-------")
+        const result = await manager.sudo.findOne<TaskEntity>("tasks", {
+            where: {
+                operator: OperatorEnum.And,
+                filters: [
+
+                    {
+                        field: 'id',
+                        comparator: ComparatorEnum.In,
+                        value: 1,
+                    },
+                ],
+            }
+        })
+
+        console.log("RESULT: ", result)
 
         return "JOB WORKS";
     }
