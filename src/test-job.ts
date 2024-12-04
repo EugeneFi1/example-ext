7a6baae33f267a608990a11f8aaa407339abb2eb
Yevhenii <yevhenii.filonenko@modern-expo.com>
2024-12-04 11:21:36 +0200

    test

--- src/test-job.ts
+++ src/test-job.ts
@@ -10,13 +10,26 @@ export class TestJob {
 
     @SubscribeJob()
     public async run(manager: Manager): Promise<void> {
-        console.log("------------TEST JOB-------")
+        console.log("------------TEST JOB run-------")
         const result = await manager.sudo.findOneById<TaskEntity>("tasks", 1, {
             id: true,
             title: true
         })
 
         console.log(" TEST JOB RESULT: ", result)
+
+        return;
+    }
+
+    @SubscribeJob()
+    public async execute(manager: Manager): Promise<void> {
+        console.log("------------TEST JOB execute-------")
+        const result = await manager.sudo.findOneById<TaskEntity>("tasks", 2, {
+            id: true,
+            title: true
+        })
+
+        console.log(" TEST JOB execute RESULT: ", result)
 
         return;
     }
