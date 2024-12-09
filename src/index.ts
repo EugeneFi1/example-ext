aa1fff8d86532605b6f9ec37022d7e0d48cfb955
Daniel <d.kolesnyk@gmail.com>
2024-12-09 15:10:43 +0200

    --

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import { ExampleController } from './example.controller';
 import { TaskExtension } from './task.extension';
 import { ExampleJob } from './example.job';
 import { Module } from '@nestjs/common';
-import { DiscoveryModule } from '@nestjs/core';
 
 export { TaskExtension, ExampleController, ExampleJob };
 
 @Module({
-	imports: [DiscoveryModule],
+	imports: [],
 	controllers: [ExampleController],
 	providers: [TaskExtension],
 	exports: [],
