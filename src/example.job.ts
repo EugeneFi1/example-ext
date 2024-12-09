0dd50f5d9ec34c3bb18a9bb8ffead306c5b04fe4
Daniel <d.kolesnyk@gmail.com>
2024-12-09 15:48:04 +0200

    --

--- src/example.job.ts
+++ src/example.job.ts
@@ -1,5 +1,7 @@
 import { Job, Manager, SubscribeJob } from '@suppa/sdk';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 @Job('test')
 export class ExampleJob {
 	@SubscribeJob()
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@ import { ExampleController } from './example.controller';
 import { TaskExtension } from './task.extension';
 import { ExampleJob } from './example.job';
 import { Module } from '@nestjs/common';
+import { DiscoveryModule } from '@nestjs/core';
 
 export { TaskExtension, ExampleController, ExampleJob };
 
 @Module({
-	imports: [],
+	providers: [ExampleJob],
+})
+class DiffModule {}
+
+@Module({
+	imports: [DiscoveryModule, DiffModule],
 	controllers: [ExampleController],
 	providers: [TaskExtension],
 	exports: [],
